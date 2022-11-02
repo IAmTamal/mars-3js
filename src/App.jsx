543d2e95c088e8c92737226c5fa1629b4e051a13
Tamal Das <gyansujan69@gmail.com>
2022-11-02 23:21:05 +0530

    feat : Added ev, controls

--- src/App.jsx
+++ src/App.jsx
@@ -2,18 +2,21 @@ import { Canvas } from '@react-three/fiber'
 import React from 'react'
 import Sphere1 from './components/Sphere1'
 import "./App.css"
+import { Environment, useEnvironment } from '@react-three/drei'
 
 
 
 const App = () => {
 
+  const outerspace = useEnvironment({ files: "/assets/nebula.hdr" })
+
   return (
     <Canvas className='main_canvas'>
 
-      <ambientLight intensity={0.7} />
-      <directionalLight position={[10, 10, 5]} />
+      <ambientLight intensity={0.8} />
+      {/* <directionalLight position={[-2, 1, 0.8]} intensity={0.6} /> */}
 
-
+      <Environment map={outerspace} background />
 
       <Sphere1 />
     </Canvas>
--- src/components/Sphere1.jsx
+++ src/components/Sphere1.jsx
@@ -1,7 +1,11 @@
-import { useThree } from '@react-three/fiber';
-import React, { useEffect } from 'react'
+import { useFrame, useThree } from '@react-three/fiber';
+import React, { useEffect, useRef } from 'react'
 import { OrbitControls } from "three/examples/jsm/controls/OrbitControls"
 
+import { useLoader } from '@react-three/fiber';
+import { TextureLoader } from 'three';
+import texture from "../assets/map.jpg"
+import { Environment, Sphere } from '@react-three/drei';
 const Sphere1 = () => {
 
     const { camera, gl } = useThree();
@@ -10,18 +14,45 @@ const Sphere1 = () => {
             const controls = new OrbitControls(camera, gl.domElement);
             controls.minDistance = 3;
             controls.maxDistance = 20;
+            controls.enableZoom = false;
+            controls.autoRotate = true;
+            controls.enableDamping = true;
+            controls.dampingFactor = 0.5;
+            controls.rotateSpeed = 0.4;
+            controls.panSpeed = 0.4;
+
             return () => {
+
                 controls.dispose();
             };
         },
         [camera, gl]
     );
-    return (
-        <mesh rotation={[90, 0, 20]}>
-            <boxGeometry args={[2.5, 2.5, 2.5]} />
 
-            <meshStandardMaterial color='hotpink' />
+    const texture1 = useLoader(TextureLoader, texture);
+
+    const marsRef = useRef();
+
+    useFrame(() => {
+
+        marsRef.current.rotation.y += 0.001;
+
+    })
+
+    return (
+        <mesh ref={marsRef} >
+            <sphereGeometry args={[2.5, 32, 32]} />
+
+            {/* <meshStandardMaterial map={texture1} /> */}
+            <meshPhongMaterial map={texture1} />
         </mesh>
+
+
+
+
+        // <Sphere args={[1, 100, 200]} scale={2.5} ref={marsRef}>
+        //     <meshStandardMaterial map={texture1} />
+        // </Sphere>
     )
 }
 
